3c89d9fc4e0c52337f1f71d989a8ced93744f10c
Ismaiel Mahmoud <som3a1941998@gmail.com>
2023-07-26 14:38:44 +0300

    seconed commit

---
+++ src/components/AddTask.vue
@@ -0,0 +1,96 @@
+<template>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Name</label>
+      <input type="text" v-model="name" name="text" placeholder="Add name" />
+    </div>
+    <div class="form-control">
+      <label>Relative Relation</label>
+      <input
+        type="text"
+        v-model="relation"
+        name="day"
+        placeholder="Add Relative Relation "
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Importance</label>
+      <input type="checkbox" v-model="important" name="reminder" />
+    </div>
+
+    <input type="submit" value="Save Person" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data() {
+    return {
+      name: '',
+      relation: '',
+      important: false,
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+
+      if (!this.name) {
+        alert('Please add a name')
+        return
+      }
+
+      const newTask = {
+        // id: Math.floor(Math.random() * 100000),
+        name: this.name,
+        relation: this.relation,
+        important: this.important,
+      }
+
+      this.$emit('add-task', newTask)
+
+      this.name = ''
+      this.relation = ''
+      this.important = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 15/7/2023</p>
+    <div v-show="homePage">
+      <router-link to="/about" >About</router-link>
+      
+    </div>
+  </footer>
+</template>
+<script>
+export default{
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
